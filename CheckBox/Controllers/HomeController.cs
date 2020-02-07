9fa4aa45e8ded53142d9b5d0fa34c2d33088895e
Genada Zoto <genadazoto@hotmail.com>
2020-02-07 12:38:01 +0100

    checkbox

---
+++ CheckBox/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CheckBox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckBox.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            string[] values= {"not good", "good", "very good"};
+            List<string> Liste = new List<string>(values);
+           
+            return View(Liste);
+        }
+        
+        
+        public ActionResult Show()
+        {
+            return View(this.Request.Form);
+            
+        }
+     
+
+    }
+}
---
+++ CheckBox/Models/LevelValues.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckBox.Models
+{
+    public class LevelValues
+    {
+        private string _value1;
+        private string _value2;
+        private string _value3;
+
+        public string Value1
+        {
+            get
+            {
+                return _value1;
+            }
+
+            set
+            {
+                _value1 = "not good";
+            }
+        }
+
+        public string Value2
+        {
+            get
+            {
+                return _value2;
+            }
+
+            set
+            {
+                _value2 = "good";
+            }
+        }
+
+        public string Value3
+        {
+            get
+            {
+                return _value3;
+            }
+
+            set
+            {
+                _value3 = "very good";
+            }
+        }
+    }
+}
